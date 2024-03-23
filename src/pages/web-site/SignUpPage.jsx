b1efe48fc3916fc578cfd7c959f0d7fa1453fcb9
balijawa hussein <balijawahussein@gmail.com>
2024-03-23 18:37:09 +0300

    inputs side taking shappe

--- src/pages/web-site/SignUpPage.jsx
+++ src/pages/web-site/SignUpPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ajoLogo2 } from '../../../public/assets';
 import Inputs from '../../components/inputs/Main-Inputs';
 import Mainheader from '../../components/headers/Main-header';
+import Paragraph from '../../components/paragraphs';
 
 const SignUpPage = () => {
   return (
@@ -14,7 +15,8 @@ const SignUpPage = () => {
             </div>
         </div>
         <div className="input-side">
-          <Mainheader />
+          <Mainheader headertext="create a secure a account" />
+          <Paragraph class="main-paragraph" ParagraphText="easily meet your saving goals with ajovault"/>
           <div className="user-form">
            <form action="/" method=''>
             <Inputs label="Full Name" type="text" id="fullName" placeholder="enter your first name then last name"/>
