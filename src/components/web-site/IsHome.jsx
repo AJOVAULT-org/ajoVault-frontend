7c40a100971bfa4a84aecf2697a27706c7b7f0db
balijawa hussein <balijawahussein@gmail.com>
2024-04-06 17:24:38 +0300

    CREATE : landing page hero

---
+++ src/components/web-site/IsHome.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function IsHome() {
+  return (
+    <div className='isHome'>
+        <div className="isHome-hero">
+
+            <div className="text-side">
+                <h1>With your <span>Saving</span></h1>
+                <h1>Powe, You can</h1>
+                <h1>Build <span>Wealth</span></h1>
+               <p>Join other wise Nigerians to revolutionize
+                their personal finance, by combining
+                individual savings and collaborative
+                pooled contributions.</p>
+            </div>
+
+            <div className="img-side">
+
+            </div>
+        </div>
+        <Link to={"/signup"}>
+            <button className="bg-primary lg:px-12 px-4 py-4 rounded-[8px] text-white lg:text-[24px] font-semibold">
+              Get Started
+            </button>
+          </Link>
+    </div>
+  )
+}
+
+export default IsHome
