dc9fdb355a9a1ca87b4a037d5515e94c82871e98
iruoje faith <faithiruoje@gmail.com>
2024-04-08 03:27:33 +0100

    features/home/Dashboard

--- src/components/headers/Main-header.jsx
+++ src/components/headers/Main-header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Mainheader(prop) {
   return (
     <div className={prop.class}>
-        <h1>{prop.headertext}</h1>
+      <h1>{prop.headertext}</h1>
     </div>
   )
 }
