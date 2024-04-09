6f9824dfb2a6ac6e24b7635de32c2149fd231113
balijawa hussein <balijawahussein@gmail.com>
2024-04-09 16:40:24 +0300

    UPDATE : CARDS PROPS  ARE DONE

--- src/components/Cards/Products-Card.jsx
+++ src/components/Cards/Products-Card.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import CardText from './CardText'
-import SmallHeaders from '../headers/headers/SmallHeaders'
-import Paragraph from '../paragraphs'
+import React from 'react';
+import SmallHeaders from '../headers/headers/SmallHeaders';
+import Paragraph from '../paragraphs';
 
 function ProductsCard(props) {
   return (
     <div className='card'>
         <div className="icon"></div>
-        <div className="text">
-        <SmallHeaders headerText={props.headerText}/>
+        
+        <div className="card-text">
+        <SmallHeaders headertext={props.headerText} class="card-headers"/>
         <Paragraph ParagraphText={props.ParagraphText}/>
         </div>
     </div>
-  )
+  );
 }
 
 export default ProductsCard;
--- src/components/web-site/Products.jsx
+++ src/components/web-site/Products.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import SmallHeaders from '../headers/headers/SmallHeaders'
-import ProductsCard from '../Cards/Products-Card'
+import React from 'react';
+import SmallHeaders from '../headers/headers/SmallHeaders';
+import ProductsCard from '../Cards/Products-Card';
+
 function Products() {
   return (
     <div className='products-section'>
         <SmallHeaders headertext="products" class="products-header"/>
         <div className="products">
-            <ProductsCard headerText="individual savings" ParagraphText="automated savings goal setting option"/>
-            <ProductsCard headerText="individual savings" ParagraphText="automated savings goal setting option"/>
-            <ProductsCard headerText="individual savings" ParagraphText="automated savings goal setting option"/>
-            <ProductsCard headerText="individual savings" ParagraphText="automated savings goal setting option"/>
-            <ProductsCard headerText="individual savings" ParagraphText="automated savings goal setting option"/>
+            <ProductsCard headerText="individual savings" ParagraphText="automated savings plans with goal setting option"/>
+            <ProductsCard headerText="individual savings" ParagraphText="automated savings plans with goal setting option"/>
+            <ProductsCard headerText="individual savings" ParagraphText="automated savings plans with goal setting option"/>
+            <ProductsCard headerText="individual savings" ParagraphText="automated savings plans with goal setting option"/>
+            <ProductsCard headerText="individual savings" ParagraphText="automated savings plans with goal setting option"/>
         </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
