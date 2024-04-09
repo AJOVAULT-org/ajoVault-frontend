3156ae4e5c4328566956e5b77b3151816ec24583
balijawa hussein <balijawahussein@gmail.com>
2024-04-09 14:04:05 +0300

    Update : products taking shape

---
+++ src/components/web-site/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SmallHeaders from '../headers/headers/SmallHeaders'
+function Products() {
+  return (
+    <div>
+        <SmallHeaders headertext="products" class="products-header"/>
+        <div className="products">
+        </div>
+    </div>
+  )
+}
+
+export default Products
